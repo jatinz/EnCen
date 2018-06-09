59d3b2d6ea808c3b17d2c6c285004f4b931677dc
root <jatinzpatro24@gmail.com>
2018-06-10 01:20:40 +0530

    #2: Added Exit Button, Working on Event Handling and loading Messages.

--- EnCen/src/org/Home.java
+++ EnCen/src/org/Home.java
@@ -6,6 +6,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.NavigationEntry;
+import com.teamdev.jxbrowser.chromium.events.FailLoadingEvent;
+import com.teamdev.jxbrowser.chromium.events.FinishLoadingEvent;
+import com.teamdev.jxbrowser.chromium.events.FrameLoadEvent;
+import com.teamdev.jxbrowser.chromium.events.LoadAdapter;
+import com.teamdev.jxbrowser.chromium.events.LoadEvent;
+import com.teamdev.jxbrowser.chromium.events.NetError;
+import com.teamdev.jxbrowser.chromium.events.ProvisionalLoadingEvent;
+import com.teamdev.jxbrowser.chromium.events.StartLoadingEvent;
 import com.teamdev.jxbrowser.chromium.swing.BrowserView;
 
 public class Home {
@@ -13,6 +22,7 @@ public class Home {
 	private static JFrame frame = null;
 	private static Browser browser = null;
 	private static BrowserView view = null;
+	private static JLabel labelDisplay;
 	
 	public static void main(String[] args) {
 		GraphicsDevice device = GraphicsEnvironment
@@ -33,11 +43,77 @@ public class Home {
 		            
 		        }
 		    });
-		//view.add(exitButton,BorderLayout.SOUTH);
-        frame.add(view,BorderLayout.CENTER);
+		
+		 
         browser.loadURL("http://www.google.com");
-       
+        labelDisplay = new JLabel("Loading Webpage ...");
+        
+        //Add to the Browser view-------------------
+        
+        view.add(labelDisplay,BorderLayout.NORTH);
+        view.add(exitButton,BorderLayout.SOUTH);
+        
+        //-------------------------------------------
+        
+        int entryCount = browser.getNavigationEntryCount();
+        int index = browser.getCurrentNavigationEntryIndex();
+		NavigationEntry navigationEntry = browser.getNavigationEntryAtIndex(index);
+        System.out.println("URL = " + navigationEntry.getURL());
+        System.out.println("Original URL = " + navigationEntry.getOriginalURL());
+        System.out.println("Title = " + index);
+        browser.addLoadListener(new LoadAdapter() {
+            @Override
+            public void onStartLoadingFrame(StartLoadingEvent event) {
+                if (event.isMainFrame()) {
+                    System.out.println("Main frame has started loading");
+                    view.remove(labelDisplay);
+                }
+            }
 
+            @Override
+            public void onProvisionalLoadingFrame(ProvisionalLoadingEvent event) {
+                if (event.isMainFrame()) {
+                    System.out.println("Provisional load was committed for a frame");
+                    view.remove(labelDisplay);
+                }
+            }
+
+            @Override
+            public void onFinishLoadingFrame(FinishLoadingEvent event) {
+                if (event.isMainFrame()) {
+                    System.out.println("Main frame has finished loading");
+                    view.remove(labelDisplay);
+                }
+            }
+
+            @Override
+            public void onFailLoadingFrame(FailLoadingEvent event) {
+                NetError errorCode = event.getErrorCode();
+                if (event.isMainFrame()) {
+                    System.out.println("Main frame has failed loading: " + errorCode);
+                    labelDisplay = new JLabel("Error-FailLoadingEvent : Encountered Error. Check Logs.");
+                    //labelLoading.setBounds(100, 199, 200, 50);
+                    view.add(labelDisplay,BorderLayout.NORTH);
+                }
+            }
+
+            @Override
+            public void onDocumentLoadedInFrame(FrameLoadEvent event) {
+                System.out.println("Frame document is loaded.");
+                view.remove(labelDisplay);
+            }
+
+            @Override
+            public void onDocumentLoadedInMainFrame(LoadEvent event) {
+                System.out.println("Main frame document is loaded.");
+                view.remove(labelDisplay);
+            }
+        });
+
+
+
+       
+        frame.add(view,BorderLayout.CENTER);
 	}
 
 	private static JFrame createFrame() {
